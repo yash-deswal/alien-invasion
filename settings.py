a96cc235dab450b20c87a93203dc44afffa4e250
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2025-03-11 17:05:14 +0530

    Scoring
    - Adding the play button
    - Leveling up
    - Scoring

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion"""
     
     def __init__(self):
-        """Initialise the game's settings."""
+        """Initialise the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 750
@@ -22,5 +22,31 @@ class Settings:
         # Alien Settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+        
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """Initialise settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # Scoring settings
+        self.alien_points = 50
+    
+    def increase_speed(self):
+        """Increase speed settings and alien point values."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
